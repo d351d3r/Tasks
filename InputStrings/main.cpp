0c2d19fff61cb76795b7b9b95d055906831ecff6
Anatolii Cherednichenko <d351d3r@desidesoft.dev>
2020-09-09 09:16:40 +0000

    Create main.cpp

---
+++ InputStrings/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+int main()
+{
+    std::ofstream out;          /
+    out.open("hello.txt"); 
+    if (!out) {
+        std::cout << "Cannot open file.\n";
+        return 1;
+    }
+
+    if (out.is_open())
+    {
+            std::cout << "Enter the word:\n";
+            std::cin.ignore();
+            std::string line;
+        while ((getline(std::cin, line)) && !line.empty())
+        {
+                out << line;
+        }
+    }
+
+    return 0;
+}
